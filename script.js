a1c3125b63eb6f657fe8325d0b725fc427bd5757
rohitupskills <130683027+rohitupskills@users.noreply.github.com>
2023-11-14 21:06:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+ function action(){
+            let fName = document.querySelector('.firstName').value;
+            let lName = document.querySelector('.lastName').value;
+            let pNumber = document.querySelector('.phoneNumber').value;
+            let email = document.querySelector('.emialId').value;
+           alert(`First Name:${fName} \nLast Name: ${lName}\nPhone Number: ${pNumber}\nEmail ID:${email}`)
+        }
